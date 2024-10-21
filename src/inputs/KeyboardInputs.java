55151c8c8675e3f6ec2a063ccff19a070cb2e5d9
Yago Guirra <yagoguirra@hotmail.com>
2024-10-21 14:30:18 -0300

    adição da sprite e logica para movimentação, uso de constantes também

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -5,6 +5,8 @@ import main.GamePanel;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import static utilz.Constants.Directions.*;
+
 public class KeyboardInputs implements KeyListener {
     private GamePanel gamePanel;
 
@@ -19,23 +21,30 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_D:
+                gamePanel.setMoving(false);
+                break;
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-                gamePanel.changeYDelta(-5);
+                gamePanel.setDirection(UP);
                 break;
             case KeyEvent.VK_A:
-                gamePanel.changeXDelta(-5);
+                gamePanel.setDirection(LEFT);
                 break;
             case KeyEvent.VK_S:
-                gamePanel.changeYDelta(5);
+                gamePanel.setDirection(DOWN);
                 break;
             case KeyEvent.VK_D:
-                gamePanel.changeXDelta(5);
+                gamePanel.setDirection(RIGHT);
                 break;
         }
 
--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -46,6 +46,5 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        //gamePanel.setRectPos(e.getX(), e.getY());
     }
 }
